f457d282379a5531fcc6dd1fb6fae69813026dd0
BT_C09_33 <mobileappdev@bths.edu>
2026-04-22 13:33:24 -0400

    added a fact about me

--- Task30/ContentView.swift
+++ Task30/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
             Text("My name is Leo")
             Text("My birthday is Feb 6")
             Text("My favorite color is blue")
-        
+            Text("I have a sister who is currently in college")
         }
         .padding()
     }
